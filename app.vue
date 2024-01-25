5e78b7afd2c8d0c7a5ec790ed9cf00ce3e30d589
FangChenYi <fx1.github@gmail.com>
2024-01-25 17:37:45 +0800

    first commit

---
+++ app.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import './assets/styles/main.scss'
+import HeaderComponents from './components/Header/HeaderComponents.vue';
+</script>
+
+<template>
+  <div class="header-container">
+    <div class="global-container">
+      <header>
+        <HeaderComponents />
+      </header>
+    </div>
+  </div>
+  <main></main>
+</template>
+
+<style lang="scss" scoped>
+.global-container {
+  width: 1200px;
+  margin: auto;
+}
+
+.header-container {
+  background-color: #ccc;
+}
+</style>
